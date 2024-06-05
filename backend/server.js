df8d605f6f98dc237afd9b8750830f41439c904c
Kashyap <kashyappadiya77@gmail.com>
2024-06-06 01:11:44 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions.js");
+
+const { logger } = require("./middleware/logger.js");
+const errorHandler = require("./middleware/errorHandler.js");
+const PORT = process.env.PORT || 3000;
+
+console.log(process.env.NODE_ENV);
+
+// app.use("/", express.static(path.join(__dirname, "public")));
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static("public"));
+
+app.use("/", require("./routes/root.js"));
+
+app.all("*", (req, res) => {
+    res.status(404);
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "Error404.html"));
+    } else if (req.accepts("json")) {
+        res.json({ message: "404 Not Found" });
+    } else {
+        res.type("txt").send("404 Not Found");
+    }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
